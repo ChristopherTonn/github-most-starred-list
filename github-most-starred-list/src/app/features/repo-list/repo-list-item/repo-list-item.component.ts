11c050fcd4abcc8c523dcf2379e9da5405bdd42b
Christopher Tonn <christopher.tonn@gmail.com>
2025-08-11 22:55:16 +0200

    • CHANGE input prop for list itema and added the date pipe.

--- github-most-starred-list/src/app/features/repo-list/repo-list-item/repo-list-item.component.ts
+++ github-most-starred-list/src/app/features/repo-list/repo-list-item/repo-list-item.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { Repo } from '../../../core/models/repo.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-repo-list-item',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './repo-list-item.component.html',
   styleUrl: './repo-list-item.component.css',
 })
 export class RepoListItemComponent {
-  @Input() item: any;
+  @Input() repo!: Repo;
 }

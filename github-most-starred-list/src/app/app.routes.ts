46eaa8ed5e8be8b9f6a4a8adfb9e5775c0ba1025
Christopher Tonn <christopher.tonn@gmail.com>
2025-08-11 17:18:34 +0200

    • ADD router to the app and made the list component accessable.

--- github-most-starred-list/src/app/app.routes.ts
+++ github-most-starred-list/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/repo-list/repo-list.component').then((m) => m.RepoListComponent),
+  },
+  {
+    path: '',
+    redirectTo: '/repos',
+    pathMatch: 'full',
+  },
+];

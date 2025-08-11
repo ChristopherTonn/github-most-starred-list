eefb7ac4c952bf42c5ef4e0a7d905d59a9390fb1
Christopher Tonn <christopher.tonn@gmail.com>
2025-08-11 22:59:41 +0200

    • CHANGE did some modifications and configurations to make the e2e tests running.

--- github-most-starred-list/src/app/app.component.spec.ts
+++ github-most-starred-list/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, AppComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule, AppComponent],
     }).compileComponents();
   });
 
@@ -24,7 +25,8 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('github-most-starred-list');
+
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });

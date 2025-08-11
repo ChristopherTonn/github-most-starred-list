41c82e3fcfaac8eacc2741ad99e7302464c833bf
Christopher Tonn <christopher.tonn@gmail.com>
2025-08-11 23:01:52 +0200

    • CHANGE adjust e2e test for list and list item.

--- github-most-starred-list/cypress/e2e/repo-list-item.cy.ts
+++ github-most-starred-list/cypress/e2e/repo-list-item.cy.ts
@@ -1,25 +1,47 @@
 describe('Repo List Item', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit('/repos');
+
+    cy.contains('Most starred github repositories', { timeout: 10000 }).should('be.visible');
+
+    cy.get('app-repo-list-item, li.list-row, [class*="repo-item"]', { timeout: 10000 }).should(
+      'have.length.at.least',
+      1
+    );
   });
 
   it('should display repo list items', () => {
-    cy.get('app-repo-list-item').should('exist');
+    cy.get('app-repo-list-item, li.list-row, [class*="repo-item"]').should(
+      'have.length.at.least',
+      1
+    );
   });
 
   it('should show profile picture', () => {
-    cy.get('app-repo-list-item img').should('be.visible');
+    cy.get('app-repo-list-item img, li img, [class*="repo-item"] img')
+      .first()
+      .should('be.visible')
+      .and('have.attr', 'src');
   });
 
   it('should display title', () => {
-    cy.get('.repo-list-item__title').should('contain.text', 'Titel');
+    cy.get('app-repo-list-item, li.list-row, [class*="repo-item"]')
+      .first()
+      .invoke('text')
+      .should('not.be.empty');
   });
 
   it('should display description', () => {
-    cy.get('.repo-list-item__description').should('contain.text', 'Description');
+    cy.get('app-repo-list-item, li.list-row, [class*="repo-item"]')
+      .first()
+      .invoke('text')
+      .should('match', /[A-Za-z\s]{10,}/);
   });
 
-  it('should show stars badge', () => {
-    cy.get('.badge--stars').should('contain.text', 'Stars');
+  it('should show stars count', () => {
+    cy.get('app-repo-list-item, li.list-row, [class*="repo-item"]')
+      .first()
+      .invoke('text')
+      .should('match', /\d+/);
   });
 });
--- github-most-starred-list/cypress/e2e/repo-list.cy.ts
+++ github-most-starred-list/cypress/e2e/repo-list.cy.ts
@@ -4,12 +4,13 @@ describe('Repo List', () => {
   });
 
   it('should display the repo list page', () => {
-    cy.get('.github-section').should('be.visible');
-    cy.contains('Most starred github repositories of the last 30 days').should('be.visible');
+    cy.get('[class*="github"], [class*="repo-list"], main, section').should('be.visible');
+
+    cy.contains('Most starred github repositories').should('be.visible');
   });
 
   it('should show at least one list item', () => {
-    cy.get('.github-section__repo-list li.list-row', { timeout: 10000 }).should(
+    cy.get('app-repo-list-item, li, [class*="repo-item"]', { timeout: 10000 }).should(
       'have.length.at.least',
       1
     );
